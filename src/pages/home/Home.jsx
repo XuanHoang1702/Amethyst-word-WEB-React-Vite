030f1b6c438fb2ddf97e994263667b0685fae950
hoaiiann0804 <hoaiiann0804@gmail.com>
2025-06-13 21:16:50 +0700

    trang homehome

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,29 @@
+import React from 'react';
 import Banner from '../../components/layout/Banner';
 
-import BannerSection from '../../components/layout/BannerSection';
-import BrandLogos from '../brand/BrandLogo';
+import WhyUs from '../../components/layout/WhyUs';
 import CategoryList from '../categories/CategoryList';
 import { ProductList } from '../products';
 import BestSellerList from '../products/best_seller/BestSellerList';
+import bannermiddle from '../../assets/image/banner-thoi-trang-nu.jpg'
 import SaleList from '../products/sale/SaleList';
+import SaleBanner from '../../components/layout/BannerSale';
+import BlogPostCard from '../Blog/BlogPostCard';
+import samplePosts from '../../service/BlogService';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import BannerSection from '../../components/layout/BannerSection';
+import BrandLogos from '../brand/BrandLogo';
 const Home = () => {
     return (
 <div className="justify-center items-center max-w-full mx-auto"> 
     <div className="flex flex-col justify-center items-center ">
       <Banner />
     </div>
-  <div className="w-full my-10">
+  <div className="w-full my-1">
   <CategoryList/>
-  <div className="justify-center items-center max-w-full">
+  
+  <div className="justify-center items-center max-w-full ">
     <BrandLogos />
     </div>
   <section>
@@ -46,23 +55,7 @@ const Home = () => {
         ))}
       </div>
   </section>
-      <div className="justify-center items-center max-w-full p-20"> 
-          <div className="flex flex-col justify-center items-center ">
-            <Banner />
-          </div>
-          <div className="w-full my-7">
-            <BrandLogos/>
-            <BannerSection/>
-            <CategoryList/>
-            <section> 
-              <ProductList />
-            </section>
-            <div className="justify-center items-center max-w-full">
-              <BestSellerList/>
-            </div>
-            <div className="justify-center items-center max-w-full">
-              <SaleList/>
-            </div>
+      
           </div>
         </div>
       );
