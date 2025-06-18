00062cc33c0b8b352528a575e71fd083a3dcf8be
XuanHoang1702 <tranxuanhoang17022004@gmail.com>
2025-06-18 22:14:06 +0700

    Update Code more function

--- src/pages/products/ProductSort.jsx
+++ src/pages/products/ProductSort.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
 
 const ProductSort = ({ sortBy, onSortChange }) => {
   return (
     <div className="hidden md:flex items-center gap-2 text-sm">
       <span className="mx-2">|</span>
-      <span>Sort by:</span>
+      <span>Sắp xếp:</span>
       <select 
         className="border-none bg-transparent font-medium focus:outline-none"
         value={sortBy}
         onChange={(e) => onSortChange(e.target.value)}
       >
-        <option value="default">Sắp xếp theo giá tiền </option>
-        <option value="price-asc">Giá tiền: Từ thấp đến cao</option>
-        <option value="price-desc">Giá tiền: Từ cao đến thấp</option>
+        <option value="default">Theo giá tiền </option>
+        <option value="price-asc">Từ thấp đến cao</option>
+        <option value="price-desc">Từ cao đến thấp</option>
       </select>
     </div>
   );
